9341aca598d7d41ee4f018e642c268b9a9dc17dd
Federated Compute Team <federated-compute-team@google.com>
2022-03-15 10:15:16 -0700

    Automated code cleanup
    PiperOrigin-RevId: 434785443

--- fcp/tensorflow/testing/tf_helper.h
+++ fcp/tensorflow/testing/tf_helper.h
@@ -37,39 +37,6 @@ namespace fcp {
  */
 absl::Cord CreateGraph(tensorflow::Scope* root);
 
-/**
- * Use a basic TfSession to create a checkpoint with a specified value from a
- * specified variable.
- */
-template <typename T>
-Result<absl::Cord> GetCheckpoint(const std::string& var_name,
-                                 tensorflow::DataType dt,
-                                 tensorflow::PartialTensorShape shape,
-                                 const tensorflow::Input::Initializer& value) {
-  // Construct a TensorFlow graph with all desired operations.
-  tensorflow::Scope root = tensorflow::Scope::NewRootScope();
-  auto v = tensorflow::ops::Variable(root, shape, dt);
-  // TODO(team): Consider breaking this function into templated and
-  //  non-templated parts to reduce code size.
-  auto init = tensorflow::ops::Assign(root.WithOpName("init"), v,
-                                      tensorflow::ops::Const<T>(root, value));
-  // Save the value of the variable in a serialized checkpoint.
-  auto filename = tensorflow::ops::Placeholder(root.WithOpName("filename"),
-                                               tensorflow::DT_STRING);
-  auto save_a =
-      tensorflow::ops::Save(root.WithOpName("save"), filename, {var_name},
-                            std::initializer_list<tensorflow::Input>{v});
-
-  // Run a session using the graph constructed above.
-  TfSession session(::testing::TempDir(), CreateGraph(&root));
-  FCP_TRY(session.RunOp("init"));
-  // Create the checkpoint.
-  google::internal::federated::plan::CheckpointOp save_checkpoint_op;
-  save_checkpoint_op.mutable_saver_def()->set_save_tensor_name("save");
-  save_checkpoint_op.mutable_saver_def()->set_filename_tensor_name("filename");
-  return session.SaveState(save_checkpoint_op);
-}
-
 }  // namespace fcp
 
 #endif  // FCP_TENSORFLOW_TESTING_TF_HELPER_H_
