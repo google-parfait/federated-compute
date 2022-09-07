badde6de6a10b11ad6e332e37079c61ce95d0435
Chunxiang (Jake) Zheng <chunxiangzheng@google.com>
2022-09-07 11:58:21 -0700

    Create a few abstractions for secure aggregation: SecAggRunner, SecAggProtocolDelegate and SecAggRunnerFactory.
    Move the majority of the secagg logic from the GrpcFederatedProtocol to SecAggRunnerImpl so that it can be reused by HttpFederatedProtocol. gRPC specific details are packaged into the implementation of GrpcSecAggProtocolDelegate.
    
    Provide a SecAggRunnerFactory in the GrpcFederatedProtocol constructor, remove SecAggClient.  In the test, we no longer rely on checking whether SecAggClient is nullptr. Instead, we now are able to provide a mock SecAggRunner, and verify the Run method is called.
    
    PiperOrigin-RevId: 472778796

---
+++ fcp/client/secagg_runner.cc
@@ -0,0 +1,236 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "fcp/client/secagg_runner.h"
+
+#include <memory>
+#include <utility>
+#include <variant>
+#include <vector>
+
+#include "fcp/secagg/shared/aes_ctr_prng_factory.h"
+#include "fcp/secagg/shared/crypto_rand_prng.h"
+#include "fcp/secagg/shared/input_vector_specification.h"
+
+namespace fcp {
+namespace client {
+
+using ::fcp::secagg::ClientState;
+
+// Implementation of StateTransitionListenerInterface.
+class SecAggStateTransitionListenerImpl
+    : public secagg::StateTransitionListenerInterface {
+ public:
+  SecAggStateTransitionListenerImpl(
+      SecAggEventPublisher& secagg_event_publisher, LogManager& log_manager,
+      SecAggSendToServerBase& secagg_send_to_server_impl,
+      SecAggProtocolDelegate& secagg_protocol_delegate);
+  void Transition(secagg::ClientState new_state) override;
+
+  void Started(secagg::ClientState state) override;
+
+  void Stopped(secagg::ClientState state) override;
+
+  void set_execution_session_id(int64_t execution_session_id) override;
+
+ private:
+  SecAggEventPublisher& secagg_event_publisher_;
+  LogManager& log_manager_;
+  SecAggSendToServerBase& secagg_send_to_server_;
+  SecAggProtocolDelegate& secagg_protocol_delegate_;
+  secagg::ClientState state_ = secagg::ClientState::INITIAL;
+};
+
+SecAggStateTransitionListenerImpl::SecAggStateTransitionListenerImpl(
+    SecAggEventPublisher& secagg_event_publisher, LogManager& log_manager,
+    SecAggSendToServerBase& secagg_send_to_server_impl,
+    SecAggProtocolDelegate& secagg_protocol_delegate)
+    : secagg_event_publisher_(secagg_event_publisher),
+      log_manager_(log_manager),
+      secagg_send_to_server_(secagg_send_to_server_impl),
+      secagg_protocol_delegate_(secagg_protocol_delegate) {}
+
+void SecAggStateTransitionListenerImpl::Transition(ClientState new_state) {
+  FCP_LOG(INFO) << "Transitioning from state: " << static_cast<int>(state_)
+                << " to state: " << static_cast<int>(new_state);
+  state_ = new_state;
+  if (state_ == ClientState::ABORTED) {
+    log_manager_.LogDiag(ProdDiagCode::SECAGG_CLIENT_NATIVE_ERROR_GENERIC);
+  }
+  secagg_event_publisher_.PublishStateTransition(
+      new_state, secagg_send_to_server_.last_sent_message_size(),
+      secagg_protocol_delegate_.last_received_message_size());
+}
+
+void SecAggStateTransitionListenerImpl::Started(ClientState state) {
+  // TODO(team): Implement this.
+}
+
+void SecAggStateTransitionListenerImpl::Stopped(ClientState state) {
+  // TODO(team): Implement this.
+}
+
+void SecAggStateTransitionListenerImpl::set_execution_session_id(
+    int64_t execution_session_id) {
+  secagg_event_publisher_.set_execution_session_id(execution_session_id);
+}
+
+SecAggRunnerImpl::SecAggRunnerImpl(
+    std::unique_ptr<SecAggSendToServerBase> send_to_server_impl,
+    std::unique_ptr<SecAggProtocolDelegate> protocol_delegate,
+    SecAggEventPublisher* secagg_event_publisher, LogManager* log_manager,
+    InterruptibleRunner* interruptible_runner,
+    int64_t expected_number_of_clients,
+    int64_t minimum_surviving_clients_for_reconstruction,
+    int64_t* bytes_downloaded, int64_t* bytes_uploaded)
+    : send_to_server_impl_(std::move(send_to_server_impl)),
+      protocol_delegate_(std::move(protocol_delegate)),
+      secagg_event_publisher_(*secagg_event_publisher),
+      log_manager_(*log_manager),
+      interruptible_runner_(*interruptible_runner),
+      expected_number_of_clients_(expected_number_of_clients),
+      minimum_surviving_clients_for_reconstruction_(
+          minimum_surviving_clients_for_reconstruction),
+      bytes_downloaded_(*bytes_downloaded),
+      bytes_uploaded_(*bytes_uploaded) {}
+
+absl::Status SecAggRunnerImpl::Run(ComputationResults results) {
+  auto secagg_state_transition_listener =
+      std::make_unique<SecAggStateTransitionListenerImpl>(
+          secagg_event_publisher_, log_manager_, *send_to_server_impl_,
+          *protocol_delegate_);
+  auto input_map = std::make_unique<secagg::SecAggVectorMap>();
+  std::vector<secagg::InputVectorSpecification> input_vector_specification;
+  for (auto& [k, v] : results) {
+    if (std::holds_alternative<QuantizedTensor>(v)) {
+      FCP_ASSIGN_OR_RETURN(uint64_t modulus, protocol_delegate_->GetModulus(k));
+      // Note: std::move is used below to ensure that each QuantizedTensor
+      // is consumed when converted to SecAggVector and that we don't
+      // continue having both in memory for longer than needed.
+      auto vector = std::get<QuantizedTensor>(std::move(v));
+      if (modulus <= 1 || modulus > secagg::SecAggVector::kMaxModulus) {
+        return absl::InternalError(
+            absl::StrCat("Invalid SecAgg modulus configuration: ", modulus));
+      }
+      if (vector.values.empty())
+        return absl::InternalError(
+            absl::StrCat("Zero sized vector found: ", k));
+      int64_t flattened_length = 1;
+      for (const auto& size : vector.dimensions) flattened_length *= size;
+      auto data_length = vector.values.size();
+      if (flattened_length != data_length)
+        return absl::InternalError(
+            absl::StrCat("Flattened length: ", flattened_length,
+                         " does not match vector size: ", data_length));
+      for (const auto& value : vector.values) {
+        if (value >= modulus) {
+          return absl::InternalError(absl::StrCat(
+              "The input SecAgg vector doesn't have the appropriate "
+              "modulus: element with value ",
+              value, " found, max value allowed ", (modulus - 1ULL)));
+        }
+      }
+      input_vector_specification.emplace_back(k, flattened_length, modulus);
+      input_map->try_emplace(
+          k, absl::MakeConstSpan(vector.values.data(), data_length), modulus);
+    }
+  }
+  SecAggSendToServerBase* send_to_server_impl_raw_ptr =
+      send_to_server_impl_.get();
+  secagg_client_ = std::make_unique<secagg::SecAggClient>(
+      expected_number_of_clients_,
+      minimum_surviving_clients_for_reconstruction_,
+      std::move(input_vector_specification),
+      std::make_unique<secagg::CryptoRandPrng>(),
+      std::move(send_to_server_impl_),
+      std::move(secagg_state_transition_listener),
+      std::make_unique<secagg::AesCtrPrngFactory>());
+
+  FCP_RETURN_IF_ERROR(interruptible_runner_.Run(
+      [this, &input_map]() -> absl::Status {
+        FCP_RETURN_IF_ERROR(secagg_client_->Start());
+        FCP_RETURN_IF_ERROR(secagg_client_->SetInput(std::move(input_map)));
+        while (!secagg_client_->IsCompletedSuccessfully()) {
+          absl::StatusOr<secagg::ServerToClientWrapperMessage>
+              server_to_client_wrapper_message =
+                  this->protocol_delegate_->ReceiveServerMessage();
+          if (!server_to_client_wrapper_message.ok()) {
+            return absl::Status(
+                server_to_client_wrapper_message.status().code(),
+                absl::StrCat(
+                    "Error during SecAgg receive: ",
+                    server_to_client_wrapper_message.status().message()));
+          }
+          auto result =
+              secagg_client_->ReceiveMessage(*server_to_client_wrapper_message);
+          if (!result.ok()) {
+            this->secagg_event_publisher_.PublishError();
+            return absl::Status(result.status().code(),
+                                absl::StrCat("Error receiving SecAgg message: ",
+                                             result.status().message()));
+          }
+          this->bytes_downloaded_ +=
+              this->protocol_delegate_->last_received_message_size();
+          if (secagg_client_->IsAborted()) {
+            std::string error_message = "error message not found.";
+            if (secagg_client_->ErrorMessage().ok())
+              error_message = secagg_client_->ErrorMessage().value();
+            this->secagg_event_publisher_.PublishAbort(false, error_message);
+            return absl::CancelledError("SecAgg aborted: " + error_message);
+          }
+        }
+        return absl::OkStatus();
+      },
+      [this]() {
+        AbortInternal();
+        this->protocol_delegate_->Abort();
+      }));
+  if (send_to_server_impl_raw_ptr) {
+    bytes_uploaded_ += send_to_server_impl_raw_ptr->total_bytes_uploaded();
+  }
+  return absl::OkStatus();
+}
+
+void SecAggRunnerImpl::AbortInternal() {
+  log_manager_.LogDiag(ProdDiagCode::SECAGG_CLIENT_NATIVE_ERROR_GENERIC);
+  auto abort_message = "Client-initiated abort.";
+  auto result = secagg_client_->Abort(abort_message);
+  if (!result.ok()) {
+    FCP_LOG(ERROR) << "Could not initiate client abort, code: " << result.code()
+                   << " message: " << result.message();
+  }
+  // Note: the implementation assumes that secagg_event_publisher
+  // cannot hang indefinitely, i.e. does not need its own interruption
+  // trigger.
+  secagg_event_publisher_.PublishAbort(true, abort_message);
+}
+
+std::unique_ptr<SecAggRunner> SecAggRunnerFactoryImpl::CreateSecAggRunner(
+    std::unique_ptr<SecAggSendToServerBase> send_to_server_impl,
+    std::unique_ptr<SecAggProtocolDelegate> protocol_delegate,
+    SecAggEventPublisher* secagg_event_publisher, LogManager* log_manager,
+    InterruptibleRunner* interruptible_runner,
+    int64_t expected_number_of_clients,
+    int64_t minimum_surviving_clients_for_reconstruction,
+    int64_t* bytes_downloaded, int64_t* bytes_uploaded) {
+  return std::make_unique<SecAggRunnerImpl>(
+      std::move(send_to_server_impl), std::move(protocol_delegate),
+      secagg_event_publisher, log_manager, interruptible_runner,
+      expected_number_of_clients, minimum_surviving_clients_for_reconstruction,
+      bytes_downloaded, bytes_uploaded);
+}
+
+}  // namespace client
+}  // namespace fcp
