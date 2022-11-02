82de3479559546e798c1bdad0c379a281950ff34
Brett McLarnon <bmclarnon@google.com>
2022-11-01 18:20:39 -0700

    Implement a FederatedDataSource for the Federated Compute demo platform.
    PiperOrigin-RevId: 485469463

---
+++ fcp/demo/federated_data_source.py
@@ -0,0 +1,110 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either expresus or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""TFF FederatedDataSource for the demo Federated Computation platform."""
+
+import dataclasses
+import functools
+import re
+from typing import Optional, Union
+
+import tensorflow as tf
+import tensorflow_federated as tff
+
+from fcp.protos import plan_pb2
+
+POPULATION_NAME_REGEX = re.compile(r'\w+(/\w+)*')
+
+_NestedExampleSelector = Union[plan_pb2.ExampleSelector,
+                               dict[str, '_NestedExampleSelector']]
+
+
+@dataclasses.dataclass
+class DataSelectionConfig:
+  population_name: str
+  example_selector: _NestedExampleSelector
+  num_clients: int
+
+
+class FederatedDataSource(tff.program.FederatedDataSource):
+  """A FederatedDataSource for use with the demo platform.
+
+  A FederatedDataSource represents a population of client devices and the set of
+  on-device data over which computations should be invoked.
+  """
+
+  _FEDERATED_TYPE = tff.FederatedType(tff.SequenceType(tf.string), tff.CLIENTS)
+
+  def __init__(self, population_name: str,
+               example_selector: _NestedExampleSelector):
+    """Constructs a new FederatedDataSource object.
+
+    Args:
+      population_name: The name of the population to execute computations on.
+      example_selector: A `plan_pb2.ExampleSelector` or a structure of
+        ExampleSelectors indicating CLIENTS-placed data to execute over.
+    """
+    if not POPULATION_NAME_REGEX.fullmatch(population_name):
+      raise ValueError(
+          f'population_name must match "{POPULATION_NAME_REGEX.pattern}".')
+    self._population_name = population_name
+    self._example_selector = example_selector
+
+  @property
+  def population_name(self) -> str:
+    """The name of the population from which examples will be retrieved."""
+    return self._population_name
+
+  @property
+  def example_selector(self) -> _NestedExampleSelector:
+    """The NestedExampleSelector used to obtain the examples."""
+    return self._example_selector
+
+  @functools.cached_property
+  def federated_type(self) -> tff.FederatedType:
+
+    def get_struct_type(value):
+      if isinstance(value, dict):
+        return tff.StructType([
+            (k, get_struct_type(v)) for k, v in value.items()
+        ])
+      # ExternalDataset always returns a sequence of tf.strings, which should be
+      # serialized `tf.train.Example` protos.
+      return tff.SequenceType(tf.string)
+
+    return tff.FederatedType(
+        get_struct_type(self._example_selector), tff.CLIENTS)
+
+  @functools.cached_property
+  def capabilities(self) -> list[tff.program.Capability]:
+    return [tff.program.Capability.SUPPORTS_REUSE]
+
+  def iterator(self) -> tff.program.FederatedDataSourceIterator:
+    return _FederatedDataSourceIterator(self)
+
+
+class _FederatedDataSourceIterator(tff.program.FederatedDataSourceIterator):
+  """A `FederatedDataSourceIterator` for use with the demo platform."""
+
+  def __init__(self, data_source: FederatedDataSource):
+    self._data_source = data_source
+
+  @property
+  def federated_type(self):
+    return self._data_source.federated_type
+
+  def select(self, num_clients: Optional[int] = None) -> DataSelectionConfig:
+    if num_clients is None or num_clients <= 0:
+      raise ValueError('num_clients must be positive.')
+    return DataSelectionConfig(self._data_source.population_name,
+                               self._data_source.example_selector, num_clients)
---
+++ fcp/demo/federated_data_source_test.py
@@ -0,0 +1,96 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either expresus or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Tests for federated_data_source."""
+
+from absl.testing import absltest
+import tensorflow as tf
+import tensorflow_federated as tff
+
+from fcp.demo import federated_data_source as fds
+from fcp.protos import plan_pb2
+
+POPULATION_NAME = 'test/name'
+EXAMPLE_SELECTOR = plan_pb2.ExampleSelector(collection_uri='app://test')
+
+
+class FederatedDataSourceTest(absltest.TestCase):
+
+  def test_invalid_population_name(self):
+    with self.assertRaisesRegex(ValueError, r'population_name must match ".+"'):
+      fds.FederatedDataSource('^invalid^', EXAMPLE_SELECTOR)
+
+  def test_population_name(self):
+    ds = fds.FederatedDataSource(POPULATION_NAME, EXAMPLE_SELECTOR)
+    self.assertEqual(ds.population_name, POPULATION_NAME)
+
+  def test_example_selector(self):
+    ds = fds.FederatedDataSource(POPULATION_NAME, EXAMPLE_SELECTOR)
+    self.assertEqual(ds.example_selector, EXAMPLE_SELECTOR)
+
+  def test_federated_type(self):
+    ds = fds.FederatedDataSource(POPULATION_NAME, EXAMPLE_SELECTOR)
+    self.assertEqual(
+        ds.federated_type,
+        tff.FederatedType(tff.SequenceType(tf.string), tff.CLIENTS))
+
+  def test_federated_type_nested(self):
+    nested_example_selector = {
+        'a': EXAMPLE_SELECTOR,
+        'b': EXAMPLE_SELECTOR,
+        'c': {
+            '1': EXAMPLE_SELECTOR,
+            '2': EXAMPLE_SELECTOR
+        },
+    }
+    ds = fds.FederatedDataSource(POPULATION_NAME, nested_example_selector)
+    self.assertEqual(
+        ds.federated_type,
+        tff.FederatedType(
+            tff.StructType([
+                ('a', tff.SequenceType(tf.string)),
+                ('b', tff.SequenceType(tf.string)),
+                ('c',
+                 tff.StructType([
+                     ('1', tff.SequenceType(tf.string)),
+                     ('2', tff.SequenceType(tf.string)),
+                 ])),
+            ]), tff.CLIENTS))
+
+  def test_capabilities(self):
+    ds = fds.FederatedDataSource(POPULATION_NAME, EXAMPLE_SELECTOR)
+    self.assertListEqual(ds.capabilities,
+                         [tff.program.Capability.SUPPORTS_REUSE])
+
+  def test_iterator_federated_type(self):
+    ds = fds.FederatedDataSource(POPULATION_NAME, EXAMPLE_SELECTOR)
+    self.assertEqual(ds.iterator().federated_type, ds.federated_type)
+
+  def test_iterator_select(self):
+    ds = fds.FederatedDataSource(POPULATION_NAME, EXAMPLE_SELECTOR)
+    self.assertEqual(
+        ds.iterator().select(10),
+        fds.DataSelectionConfig(POPULATION_NAME, EXAMPLE_SELECTOR, 10))
+
+  def test_iterator_select_with_invalid_num_clients(self):
+    ds = fds.FederatedDataSource(POPULATION_NAME, EXAMPLE_SELECTOR)
+    with self.assertRaisesRegex(ValueError, 'num_clients must be positive'):
+      ds.iterator().select(num_clients=None)
+    with self.assertRaisesRegex(ValueError, 'num_clients must be positive'):
+      ds.iterator().select(num_clients=-5)
+    with self.assertRaisesRegex(ValueError, 'num_clients must be positive'):
+      ds.iterator().select(num_clients=0)
+
+
+if __name__ == '__main__':
+  absltest.main()
