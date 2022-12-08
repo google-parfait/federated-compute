06d92a4297826e7d48bd46c0fecaa150dff1a55d
Federated Compute Team <federated-compute-team@google.com>
2022-12-08 14:22:14 -0800

    Add utilities for working with protos during Plan building.
    PiperOrigin-RevId: 493994980

---
+++ fcp/artifact_building/proto_helpers.py
@@ -0,0 +1,118 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Helper methods for proto creation logic."""
+
+from typing import Optional
+
+import tensorflow as tf
+import tensorflow_federated as tff
+
+from fcp.artifact_building import tensor_utils
+from fcp.artifact_building import type_checks
+from fcp.protos import plan_pb2
+
+
+def make_tensor_spec_from_tensor(t: tf.Tensor,
+                                 shape_hint: Optional[tf.TensorShape] = None
+                                ) -> tf.TensorSpec:
+  """Creates a `TensorSpec` from Tensor w/ optional shape hint.
+
+  Args:
+    t: A `tf.Tensor` instance to be used to create a `TensorSpec`.
+    shape_hint: A `tf.TensorShape` that provides a fully defined shape in the
+      case that `t` is partially defined. If `t` has a fully defined shape,
+      `shape_hint` is ignored. `shape_hint` must be compatible with the
+      partially defined shape of `t`.
+
+  Returns:
+    A `tf.TensorSpec` instance corresponding to the input `tf.Tensor`.
+
+  Raises:
+    NotImplementedError: If the input `tf.Tensor` type is not supported.
+    TypeError: if `shape_hint` is not `None` and is incompatible with the
+      runtime shape of `t`.
+  """
+  if not tf.is_tensor(t):
+    raise NotImplementedError('Cannot handle type {t}: {v}'.format(
+        t=type(t), v=t))
+  derived_shape = tf.TensorShape(t.shape)
+  if not derived_shape.is_fully_defined() and shape_hint is not None:
+    if derived_shape.is_compatible_with(shape_hint):
+      shape = shape_hint
+    else:
+      raise TypeError('shape_hint is not compatible with tensor ('
+                      f'{shape_hint} vs {derived_shape})')
+  else:
+    shape = derived_shape
+  return tf.TensorSpec(shape, t.dtype, name=t.name)
+
+
+def make_measurement(t: tf.Tensor, name: str,
+                     tff_type: tff.types.TensorType) -> plan_pb2.Measurement:
+  """Creates a `plan_pb.Measurement` descriptor for a tensor.
+
+  Args:
+    t: A tensor to create the measurement for.
+    name: The name of the measurement (e.g. 'server/loss').
+    tff_type: The `tff.Type` of the measurement.
+
+  Returns:
+    An instance of `plan_pb.Measurement`.
+
+  Raises:
+    ValueError: If the `dtype`s or `shape`s of the provided tensor and TFF type
+      do not match.
+  """
+  type_checks.check_type(tff_type, tff.types.TensorType)
+  if tff_type.dtype != t.dtype:
+    raise ValueError(f'`tff_type.dtype`: {tff_type.dtype} does not match '
+                     f'provided tensor\'s dtype: {t.dtype}.')
+  if tff_type.shape.is_fully_defined() and t.shape.is_fully_defined():
+    if tff_type.shape.as_list() != t.shape.as_list():
+      raise ValueError(f'`tff_type.shape`: {tff_type.shape} does not match '
+                       f'provided tensor\'s shape: {t.shape}.')
+  return plan_pb2.Measurement(
+      read_op_name=t.name,
+      name=name,
+      tff_type=tff.framework.serialize_type(tff_type).SerializeToString())
+
+
+def make_metric(v: tf.Variable, stat_name_prefix: str) -> plan_pb2.Metric:
+  """Creates a `plan_pb.Metric` descriptor for a resource variable.
+
+  The stat name is formed by stripping the leading `..../` prefix and any
+  colon-based suffix.
+
+  Args:
+    v: A variable to create the metric descriptor for.
+    stat_name_prefix: The prefix (string) to use in formulating a stat name,
+      excluding the trailing slash `/` (added automatically).
+
+  Returns:
+    An instance of `plan_pb.Metric` for `v`.
+
+  Raises:
+    TypeError: If the arguments are of the wrong types.
+    ValueError: If the arguments are malformed (e.g., no leading name prefix).
+  """
+  type_checks.check_type(stat_name_prefix, str, name='stat_name_prefix')
+  if not hasattr(v, 'read_value'):
+    raise TypeError('Expected a resource variable, found {!r}.'.format(type(v)))
+  bare_name = tensor_utils.bare_name(v.name)
+  if '/' not in bare_name:
+    raise ValueError(
+        'Expected a prefix in the name, found none in {}.'.format(bare_name))
+  stat_name = '{}/{}'.format(stat_name_prefix,
+                             bare_name[(bare_name.find('/') + 1):])
+  return plan_pb2.Metric(variable_name=v.read_value().name, stat_name=stat_name)
---
+++ fcp/artifact_building/proto_helpers_test.py
@@ -0,0 +1,151 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Tests for proto_helpers.py."""
+
+import collections
+
+import tensorflow as tf
+import tensorflow_federated as tff
+
+from fcp.artifact_building import proto_helpers
+from fcp.artifact_building import variable_helpers
+
+
+class MakeMetricTest(tf.test.TestCase):
+
+  def test_make_metric(self):
+    with tf.Graph().as_default():
+      v = variable_helpers.create_vars_for_tff_type(
+          tff.to_type(collections.OrderedDict([("bar", tf.int32)])), name="foo")
+      self.assertProtoEquals(
+          "variable_name: 'Identity:0' stat_name: 'client/bar'",
+          proto_helpers.make_metric(v[0], "client"))
+
+
+class MakeTensorSpecTest(tf.test.TestCase):
+
+  def test_fully_defined_shape(self):
+    with tf.Graph().as_default():
+      test_tensor = tf.constant([[1], [2]])  # Shape [1, 2]
+      with self.subTest("no_hint"):
+        tensor_spec = proto_helpers.make_tensor_spec_from_tensor(test_tensor)
+        self.assertProtoEquals(
+            "name: 'Const:0' "
+            "shape { "
+            "  dim { size: 2 } "
+            "  dim { size: 1 } "
+            "} "
+            "dtype: DT_INT32", tensor_spec.experimental_as_proto())
+      with self.subTest("ignored_hint"):
+        # Supplied shape hint is incompatible, but ignored because tensor is
+        # fully defined.
+        tensor_spec = proto_helpers.make_tensor_spec_from_tensor(
+            test_tensor, shape_hint=tf.TensorShape([1, 4]))
+        self.assertProtoEquals(
+            "name: 'Const:0' "
+            "shape { "
+            "  dim { size: 2 } "
+            "  dim { size: 1 } "
+            "} "
+            "dtype: DT_INT32", tensor_spec.experimental_as_proto())
+
+  def test_undefined_shape(self):
+    with tf.Graph().as_default():
+      # Create a undefined shape tensor via a placeholder and an op that doesn't
+      # alter shape.
+      test_tensor = tf.clip_by_value(
+          tf.compat.v1.placeholder(dtype=tf.int32), 0, 1)
+      with self.subTest("no_hint"):
+        tensor_spec = proto_helpers.make_tensor_spec_from_tensor(test_tensor)
+        self.assertProtoEquals(
+            "name: 'clip_by_value:0' "
+            "shape { "
+            " unknown_rank: true "
+            "} "
+            "dtype: DT_INT32", tensor_spec.experimental_as_proto())
+      with self.subTest("hint"):
+        tensor_spec = proto_helpers.make_tensor_spec_from_tensor(
+            test_tensor, shape_hint=tf.TensorShape([1, 4]))
+        self.assertProtoEquals(
+            "name: 'clip_by_value:0' "
+            "shape { "
+            "  dim { size: 1 } "
+            "  dim { size: 4 } "
+            "} "
+            "dtype: DT_INT32", tensor_spec.experimental_as_proto())
+
+  def test_partially_defined_shape(self):
+    with tf.Graph().as_default():
+      # Create a partially defined shape tensor via a placeholder and a reshape
+      # to specify some dimensions.
+      test_tensor = tf.reshape(
+          tf.compat.v1.placeholder(dtype=tf.int32), [2, -1])
+      with self.subTest("no_hint"):
+        tensor_spec = proto_helpers.make_tensor_spec_from_tensor(test_tensor)
+        self.assertProtoEquals(
+            "name: 'Reshape:0' "
+            "shape { "
+            "  dim { size: 2 } "
+            "  dim { size: -1 } "
+            "} "
+            "dtype: DT_INT32", tensor_spec.experimental_as_proto())
+      with self.subTest("hint"):
+        tensor_spec = proto_helpers.make_tensor_spec_from_tensor(
+            test_tensor, shape_hint=tf.TensorShape([2, 4]))
+        self.assertProtoEquals(
+            "name: 'Reshape:0' "
+            "shape { "
+            "  dim { size: 2 } "
+            "  dim { size: 4} "
+            "} "
+            "dtype: DT_INT32", tensor_spec.experimental_as_proto())
+      with self.subTest("invalid_hint"):
+        with self.assertRaises(TypeError):
+          _ = proto_helpers.make_tensor_spec_from_tensor(
+              test_tensor, shape_hint=tf.TensorShape([1, 4]))
+
+
+class MakeMeasurementTest(tf.test.TestCase):
+
+  def test_succeeds(self):
+    with tf.Graph().as_default():
+      tensor = tf.constant(1)
+      tff_type = tff.types.TensorType(tensor.dtype, tensor.shape)
+      m = proto_helpers.make_measurement(
+          t=tensor, name="test", tff_type=tff_type)
+
+      self.assertEqual(m.name, "test")
+      self.assertProtoEquals(
+          m.tff_type,
+          tff.framework.serialize_type(tff_type).SerializeToString())
+
+  def test_fails_for_non_matching_dtype(self):
+    with tf.Graph().as_default():
+      tensor = tf.constant(1.0)
+      tff_type = tff.types.TensorType(tf.int32, tensor.shape)
+
+      with self.assertRaisesRegex(ValueError, ".* does not match.*"):
+        proto_helpers.make_measurement(t=tensor, name="test", tff_type=tff_type)
+
+  def test_fails_for_non_matching_shape(self):
+    with tf.Graph().as_default():
+      tensor = tf.constant(1.0)
+      tff_type = tff.types.TensorType(tensor.dtype, shape=[5])
+
+      with self.assertRaisesRegex(ValueError, ".* does not match.*"):
+        proto_helpers.make_measurement(t=tensor, name="test", tff_type=tff_type)
+
+
+if __name__ == "__main__":
+  tf.test.main()
