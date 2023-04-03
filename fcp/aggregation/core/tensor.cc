5fa3a91107cb87c4a07281a8f64a42c016dc10b6
Stanislav Chiknavaryan <stanisc@google.com>
2023-04-03 14:31:51 -0700

    Prepare tensor data encoding and decoding to support DT_STRING data type
    PiperOrigin-RevId: 521564531

--- fcp/aggregation/core/tensor.cc
+++ fcp/aggregation/core/tensor.cc
@@ -64,12 +64,16 @@ StatusOr<Tensor> Tensor::Create(DataType dtype, TensorShape shape,
 
 #ifndef FCP_NANOLIBC
 
-// StringData implements TensorData by wrapping std::string and using it as
-// a backing storage.
-class StringData : public TensorData {
+// SerializedContentNumericData implements TensorData by wrapping the serialized
+// content string and using it directly as a backing storage. This relies on the
+// fact that the serialized content uses the same layout as in memory
+// representation if we assume that this code runs on a little-endian system.
+// TODO(team): Ensure little-endianness.
+class SerializedContentNumericData : public TensorData {
  public:
-  explicit StringData(std::string str) : str_(std::move(str)) {}
-  ~StringData() override = default;
+  explicit SerializedContentNumericData(std::string str)
+      : str_(std::move(str)) {}
+  ~SerializedContentNumericData() override = default;
 
   // Implementation of TensorData methods.
   size_t byte_size() const override { return str_.size(); }
@@ -79,35 +83,45 @@ class StringData : public TensorData {
   std::string str_;
 };
 
+// Converts the tensor data to a serialized blob saved as the content field
+// in the TensorProto.  The `num` argument is needed in case the number of
+// values can't be derived from the TensorData size.
 template <typename T>
-std::string EncodeContent(const TensorData* data) {
-  // Default encoding of tensor data, valid only for numeric datatypes.
+std::string EncodeContent(const TensorData* data, size_t num) {
+  // Default encoding of tensor data, valid only for numeric data types.
   return std::string(reinterpret_cast<const char*>(data->data()),
                      data->byte_size());
 }
 
+// Converts the serialized TensorData content stored in TensorProto to an
+// instance of TensorData.   The `num` argument is needed in case the number of
+// values can't be derived from the content size.
 template <typename T>
-std::unique_ptr<TensorData> DecodeContent(std::string content) {
-  // Default decoding of tensor data, valid only for numeric datatypes.
-  return std::make_unique<StringData>(std::move(content));
+std::unique_ptr<TensorData> DecodeContent(std::string content, size_t num) {
+  // Default decoding of tensor data, valid only for numeric data types.
+  return std::make_unique<SerializedContentNumericData>(std::move(content));
 }
 
 StatusOr<Tensor> Tensor::FromProto(const TensorProto& tensor_proto) {
   FCP_ASSIGN_OR_RETURN(TensorShape shape,
                        TensorShape::FromProto(tensor_proto.shape()));
+  // TODO(team): The num_values is valid only for dense tensors.
+  size_t num_values = shape.NumElements();
   std::unique_ptr<TensorData> data;
   DTYPE_CASES(tensor_proto.dtype(), T,
-              data = DecodeContent<T>(tensor_proto.content()));
+              data = DecodeContent<T>(tensor_proto.content(), num_values));
   return Create(tensor_proto.dtype(), std::move(shape), std::move(data));
 }
 
 StatusOr<Tensor> Tensor::FromProto(TensorProto&& tensor_proto) {
   FCP_ASSIGN_OR_RETURN(TensorShape shape,
                        TensorShape::FromProto(tensor_proto.shape()));
+  // TODO(team): The num_values is valid only for dense tensors.
+  size_t num_values = shape.NumElements();
   std::string content = std::move(*tensor_proto.mutable_content());
   std::unique_ptr<TensorData> data;
   DTYPE_CASES(tensor_proto.dtype(), T,
-              data = DecodeContent<T>(std::move(content)));
+              data = DecodeContent<T>(std::move(content), num_values));
   return Create(tensor_proto.dtype(), std::move(shape), std::move(data));
 }
 
@@ -115,9 +129,10 @@ TensorProto Tensor::ToProto() const {
   TensorProto tensor_proto;
   tensor_proto.set_dtype(dtype_);
   *(tensor_proto.mutable_shape()) = shape_.ToProto();
-
+  // TODO(team): The num_values is valid only for dense tensors.
+  size_t num_values = shape_.NumElements();
   std::string content;
-  DTYPE_CASES(dtype_, T, content = EncodeContent<T>(data_.get()));
+  DTYPE_CASES(dtype_, T, content = EncodeContent<T>(data_.get(), num_values));
   *(tensor_proto.mutable_content()) = std::move(content);
   return tensor_proto;
 }
