0ce308357dda87d250287094744070b3222308d3
Federated Compute Team <federated-compute-team@google.com>
2024-11-14 15:25:37 -0800

    Automated code cleanup
    PiperOrigin-RevId: 696670205

--- fcp/tensorflow/external_dataset.py
+++ fcp/tensorflow/external_dataset.py
@@ -21,7 +21,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-
 import tensorflow as tf
 
 from fcp.tensorflow import gen_external_dataset_py
