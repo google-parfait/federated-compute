2fc6c2cd0046cf7bdb86e95c754f9c6cdfd57a95
Kamil Jarosz <kjarosz@google.com>
2026-04-09 01:06:12 -0700

    Prefer ABSL_ prefixed macros
    Non-prefixed macros can conflict with other projects' internal macros.
    
    PiperOrigin-RevId: 896942765

--- fcp/confidentialcompute/crypto_test_util.cc
+++ fcp/confidentialcompute/crypto_test_util.cc
@@ -5,7 +5,7 @@
 #include <string>
 #include <utility>
 
-#include "absl/log/check.h"
+#include "absl/log/absl_check.h"
 #include "absl/status/statusor.h"
 #include "absl/strings/string_view.h"
 #include "fcp/base/monitoring.h"
@@ -18,21 +18,24 @@ namespace fcp::confidential_compute {
 std::pair<std::string, std::string> GenerateHpkeKeyPair(
     absl::string_view key_id) {
   bssl::ScopedEVP_HPKE_KEY key;
-  CHECK_EQ(EVP_HPKE_KEY_generate(key.get(), EVP_hpke_x25519_hkdf_sha256()), 1);
+  ABSL_CHECK_EQ(EVP_HPKE_KEY_generate(key.get(), EVP_hpke_x25519_hkdf_sha256()),
+                1);
 
   size_t key_len;
   std::string raw_public_key(EVP_HPKE_MAX_PUBLIC_KEY_LENGTH, '\0');
-  CHECK_EQ(EVP_HPKE_KEY_public_key(
-               key.get(), reinterpret_cast<uint8_t*>(raw_public_key.data()),
-               &key_len, raw_public_key.size()),
-           1);
+  ABSL_CHECK_EQ(
+      EVP_HPKE_KEY_public_key(key.get(),
+                              reinterpret_cast<uint8_t*>(raw_public_key.data()),
+                              &key_len, raw_public_key.size()),
+      1);
   raw_public_key.resize(key_len);
 
   std::string raw_private_key(EVP_HPKE_MAX_PRIVATE_KEY_LENGTH, '\0');
-  CHECK_EQ(EVP_HPKE_KEY_private_key(
-               key.get(), reinterpret_cast<uint8_t*>(raw_private_key.data()),
-               &key_len, raw_private_key.size()),
-           1);
+  ABSL_CHECK_EQ(
+      EVP_HPKE_KEY_private_key(
+          key.get(), reinterpret_cast<uint8_t*>(raw_private_key.data()),
+          &key_len, raw_private_key.size()),
+      1);
   raw_private_key.resize(key_len);
 
   absl::StatusOr<std::string> public_cwt = OkpCwt{
